56f4114932e94954310a597d4e05368da747edac
Malgorzata Wladyka <mwladyka126@gmail.com>
2020-10-29 20:46:03 +0100

    Change js/script, add method renderInMenu

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -54,10 +54,67 @@
     ),
   };
   class Product {
-    constructor() {
+    
+    constructor(id,data) {
       const thisProduct = this;
 
+      thisProduct.id = id,
+      thisProduct.data = data,
+
+      thisProduct.renderInMenu();
+
+      console.log('product id:', id),
+      console.log ('data:', data),
       console.log('new Product:', thisProduct);
+
+      thisProduct.initAccordion();
+    }
+
+    renderInMenu(){
+      const thisProduct=this;
+      /*generate HTML based on template*/
+
+      const generateHTML = templates.menuProduct(thisProduct.data);
+
+      console.log (generateHTML);
+
+      /*create element using utils.createElementFromHTML*/
+      thisProduct.element = utils.createDOMFromHTML (generateHTML);
+
+      console.log (thisProduct.element);
+
+      /*find menu container*/
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu*/
+      menuContainer.appendChild(thisProduct.element);
+
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+
+        /* find the clickable trigger (the element that should react to clicking) */
+
+        /* START: click event listener to trigger */
+
+          /* prevent default action for event */
+
+          /* toggle active class on element of thisProduct */
+
+          /* find all active products */
+
+          /* START LOOP: for each active product */
+
+            /* START: if the active product isn't the element of thisProduct */
+
+              /* remove class active for the active product */
+
+            /* END: if the active product isn't the element of thisProduct */
+
+          /* END LOOP: for each active product */
+
+        /* END: click event listener to trigger */
     }
   }
 
