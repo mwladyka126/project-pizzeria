f3c30267eb925cd302ef766cbd0b3f54d23dd84e
Malgorzata Wladyka <mwladyka126@gmail.com>
2020-11-17 13:27:35 +0100

    Change AmountWidget and BaseWidget. Add DatePicker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,6 @@
 import {select,templates} from './../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
 
 class Booking{
   constructor(bookingWidgetWrapper){
@@ -17,13 +18,15 @@ class Booking{
     bookingWidgetWrapper.innerHTML =generateHTML;
 
     thisBooking.dom.peopleAmount =bookingWidgetWrapper.querySelector(select.booking.peopleAmount);
-    thisBooking.dom.hoursAmount = bookingWidgetWrapper.querySelector(select.booking.hoursAmount);    
+    thisBooking.dom.hoursAmount = bookingWidgetWrapper.querySelector(select.booking.hoursAmount);  
+    thisBooking.dom.datePicker=bookingWidgetWrapper.querySelector(select.widgets.datePicker.wrapper);  
   }
   initWidgets(){
     const thisBooking=this;
 
     thisBooking.peopleAmount = new AmountWidget (thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget (thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   }   
 }
 
